f516dfd56e08a04bcfc4951e0c453d52b0051dbc
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-07-06 23:44:12 +0530

    question

--- javaScript ES6+/eventLoop.js
+++ javaScript ES6+/eventLoop.js
@@ -9,3 +9,14 @@ Promise.resolve().then(() => {
 });
 
 console.log('Script end');
+
+
+console.log('Start');
+
+setTimeout(() => console.log('setTimeout 1'), 0);
+setTimeout(() => console.log('setTimeout 2'), 0);
+
+Promise.resolve().then(() => console.log('Promise 1'));
+Promise.resolve().then(() => console.log('Promise 2'));
+
+console.log('End');
